e319ec3af97b1213d7c4829b9d667aa3c23e87d0
Ouarem Oualid <oua1992@gmail.com>
2024-11-21 16:01:15 +0100

    finished users

--- root/admin/views.py
+++ root/admin/views.py
@@ -1,16 +1,14 @@
 import secrets
 
-from flask import url_for, redirect, render_template, flash, session
+from flask import url_for, redirect, render_template, jsonify, session, abort, request, flash
 from root.admin import admin_bp
 from flask_login import login_required
 from root.admin.forms import RegistrationForm
 from flask_weasyprint import HTML, render_pdf
-from root.models import User
+from root.models import *
 from werkzeug.security import generate_password_hash
 
 from root import database
-
-from root.auth.views import send_reset_email
 
 
 @admin_bp.get('/')
@@ -20,18 +18,36 @@ def home():
 
 @admin_bp.get('/users')
 @admin_bp.post('/users')
-# @login_required
+@login_required
 def users():
     session['endpoint'] = 'users'
     _users = User.query.filter_by(is_deleted = False).all()
+    # print(_users)
+    liste = list()
+    if _users:
+        for user in _users:
+            liste.append(user.repr(columns=['id', 'full_name', 'username','role', 'phone_number']))
+    print(liste)
+    return render_template('admin/users.html', liste=liste)
+
+
+@admin_bp.get('/buses')
+@admin_bp.post('/buses')
+@login_required
+def users():
+    session['endpoint'] = 'buses'
+    _users = Bus.query.filter_by(is_deleted = False).all()
+    # print(_users)
     liste = list()
     if _users:
         for user in _users:
             liste.append(user.repr(columns=['id', 'full_name', 'username','role', 'phone_number']))
     return render_template('admin/users.html', liste=liste)
+
+
 @admin_bp.get('/users/register')
 @admin_bp.post('/users/register')
-# @login_required
+@login_required
 def register():
     form = RegistrationForm()
 
@@ -50,13 +66,11 @@ def register():
         # send_reset_email(user, "auth_bp.verify_email", subject='Email verification)
         form = RegistrationForm()
         return redirect(url_for("admin_bp.print_credentials"))
-    else:
-        print(form.errors)
     return render_template('admin/add_user.html', form=form)
 
 
 @admin_bp.get('/print')
-# @login_required
+@login_required
 def print_credentials():
     html = render_template('admin/credentials.html',
                            username = session['username'],
@@ -64,19 +78,42 @@ def print_credentials():
     return render_pdf(HTML(string=html))
 
 
-"""
-@auth_bp.get("/reset_password/<string:token>")
-@auth_bp.post("/reset_password/<string:token>")
-def reset_password(token):
-    user = User.verify_reset_token(token)
-    if user is None:
-        flash('Il y a une erreur', 'warning')
-        return redirect(url_for('auth_bp.request_token'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.password_hash = generate_password_hash(form.new_password.data, "SHA256")
-        database.session.add(user)
-        database.session.commit()
-        flash('Votre mot de passe a été changé avec succès', 'success')
-        return redirect(url_for('auth_bp.login'))
-    return render_template("reset_password.html", form=form)"""
+@admin_bp.post('/employees/get')
+@login_required
+def get_user():
+    session['endpoint'] = 'users'
+
+    data = request.json
+    user = User.query.get(int(data['user_id']))
+    if not user:
+        abort(404)
+    return jsonify(message=f"<h4 class='h4 fw-bold'>{user.first_name} {user.last_name}</h4> \
+                        <span class='fw-bold mb-3'>Pseudonyme: </span>{user.username} <br> \
+                        <span class='fw-bold mb-3'>Rôle: </span>{user.role} <br> \
+                        <span class='fw-bold mb-3'>Numéro de téléphone: </span>{user.phone_number}"), 200
+
+
+# @admin_bp.post('/users/<int:user_id>/update')
+# # @login_required
+# def edit_user(user_id):
+#     user = User.query.filter_by(is_deleted = False).get(user_id)
+#     if user is None:
+#         abort(404)
+#     return redirect(url_for("admin_bp.users"))
+
+
+@admin_bp.get('/users/<int:user_id>/delete')
+@login_required
+def delete(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return render_template("errors/404.html", blueprint="admin_bp")
+
+    if user.is_deleted:
+        flash('Erreur', 'danger')
+        return redirect(url_for("admin_bp.users"))
+    user.is_deleted = True
+    database.session.add(user)
+    database.session.commit()
+    flash('Opération se termine avec succès', "success")
+    return redirect(url_for("admin_bp.users"))
--- root/models.py
+++ root/models.py
@@ -1,12 +1,8 @@
 from root import database as db
 from flask_login import UserMixin
 
-from datetime import datetime, timedelta
-import json, jwt
-from time import time
-from root import app
-from root import login_manager
-from flask import current_app
+from datetime import datetime
+import json
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, z):
@@ -14,7 +10,6 @@ class DateTimeEncoder(json.JSONEncoder):
             return str(z)
         else:
             return super().default(z)
-
 
 
 class User(UserMixin, db.Model):
@@ -26,11 +21,11 @@ class User(UserMixin, db.Model):
     is_deleted = db.Column(db.Boolean, default=0)
     password_hasChanged = db.Column(db.Boolean, default = False)
     username = db.Column(db.String(100), nullable = False)
-    phone_number = db.Column(db.String(10))
+    phone_number = db.Column(db.String(10), default="champ vide")
     password_hash = db.Column(db.String(256), nullable=False)
 
     def __repr__(self):
-        return '<User %r %r %r>' % self.username, self.first_name, self.last_name
+        return f'USER: <{self.username}, {self.first_name}, {self.last_name}> \n'
 
     def repr(self, columns=None):
         _dict = {
@@ -41,4 +36,169 @@ class User(UserMixin, db.Model):
             'phone_number': self.phone_number
         }
         return {key: _dict[key] for key in columns} if columns else _dict
+
+
+class Voyage(db.Model):
+    __tablename__="voyage"
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(100))
+    is_deleted = db.Column(db.Boolean, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_depart = db.Column(db.DateTime, default=datetime.utcnow)
+    date_end = db.Column(db.DateTime, default=datetime.utcnow)
+    hotel_fees = db.Column(db.Integer, default=0)
+    bus_company = db.Column(db.String(100))
+    bus_fees = db.Column(db.Integer, default=0)
+    visa_fees = db.Column(db.Integer, default=0)
+    guide_fees = db.Column(db.Integer, default=0)
+    avion_fees = db.Column(db.Integer, default=0)
+    is_plane_included = db.Column(db.Boolean, default=0)
+    is_guide_included = db.Column(db.Boolean, default=0)
+    is_hotel_included = db.Column(db.Boolean, default=0)
+
+
+class Bus(db.Model):
+    __tablename__="bus"
+    id=db.Column(db.Integer, primary_key=True)
+    company = db.Column(db.String(100))
+    driver_full_name = db.Column(db.String(100))
+    capacity = db.Column(db.Integer)
+    is_deleted = db.Column(db.Boolean, default=0)
+    contacts = db.relationship('Contact', backref="bus_contact", lazy='subquery')
+    voyages = db.relationship("")
+
+
+class Contact(db.Model):
+    __tablename__ =  "contact"
+    id=db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    address = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(100))
+    is_deleted = db.Column(db.Boolean, default=0)
+    fk_bus_id = db.Column(db.Integer, db.ForeignKey('bus.id'))
+    fk_guide_id = db.Column(db.Integer, db.ForeignKey('guide.id'))
+    fk_hotel_id = db.Column(db.Integer, db.ForeignKey('hotel.id'))
+    fk_agency_id = db.Column(db.Integer, db.ForeignKey('agency.id'))
+    fk_person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+    def __repr__(self):
+        return (f'Contact: {self.name}, '
+                f'email={self.email}, '
+                f'phone={self.phone}, '
+                f'address={self.address}'
+                f'city={self.city}, '
+                f'state={self.state}, '
+                f'is_deleted={self.is_deleted}')
+
+
+class Guide(db.Model):
+    __tablename__ = "guide"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    is_deleted = db.Column(db.Boolean, default=0)
+    sex = db.Column(db.String(10))
+    contacts = db.relationship('Contact', backref="guide_contact", lazy='subquery')
+
+
+    def __repr__(self):
+        return f'{self.name}, Contacts: {self.contacts}'
+
+
+class Hotel(db.Model):
+    __tablename__ = "hotel"
+    id = db.Column(db.Integer, primary_key=True)
+    is_deleted = db.Column(db.Boolean, default=0)
+    name = db.Column(db.String(100))
+    contacts = db.relationship('Contact', backref="hotel_contact", lazy='subquery')
+    star_rating = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return f'{self.name}, Contacts: {self.contacts}'
+
+
+class Agency(db.Model):
+    __tablename__ = "agency"
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(100))
+    is_deleted = db.Column(db.Boolean, default=0)
+    reserved_places= db.Column(db.Integer, default=0)
+    contacts = db.relationship('Contact', backref="agency_contact", lazy='subquery')
+
+    def __repr__(self):
+        return f'{self.label}, Contacts: {self.contacts}'
+
+
+class Invoice(db.Model):
+    __tablename__ = "invoice"
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    amount = db.Column(db.Integer)
+    payments = db.relationship('Payment', backref='payments_invoice', lazy='subquery')
+    fk_voyage_id = db.Column(db.Integer, db.ForeignKey('voyage.id'))
+    fk_agency_id = db.Column(db.Integer, db.ForeignKey('agency.id'))
+    invoice_type = db.Column(db.String(100))
+    entities = db.relationship('Entity', backref='invoice_entities', lazy='subquery')
+
+    def __repr__(self):
+        return (f'Date de facture={self.date},'
+                f'Montant= {self.amount}, '
+                f'Voyage={Voyage.query.get(self.fk_voyage_id)},'
+                f'Agency={Agency.query.get(self.fk_agency_id)}, '
+                f'Payments={self.payments}')
+
+
+class Payment(db.Model):
+    __tablename__ = "payment"
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    amount = db.Column(db.Integer)
+    type = db.Column(db.String(100))
+    fk_invoice_id = db.Column(db.Integer, db.ForeignKey('invoice.id'))
+
+    def __repr__(self):
+        return f'Date de payment={self.date}, Montant={self.amount}'
+
+
+class Person(db.Model):
+    __tablename__ = "person"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    is_deleted = db.Column(db.Boolean, default=0)
+    contacts = db.relationship('Contact', backref="person_contact", lazy='subquery')
+    fk_agency_id = db.Column(db.Integer, db.ForeignKey('agency.id'))
+
+
+class Include(db.Model):
+    __tablename__ = "include"
+    id = db.Column(db.Integer, primary_key=True)
+    fk_voyage_id = db.Column(db.Integer, db.ForeignKey('voyage.id'))
+    fk_guide_id = db.Column(db.Integer, db.ForeignKey('guide.id'))
+    fk_bus_id = db.Column(db.Integer, db.ForeignKey('bus.id'))
+
+class VoyageForAgency(db.Model):
+    __tablename__ = "voyage_include"
+    id = db.Column(db.Integer, primary_key=True)
+    fk_agency_id = db.Column(db.Integer, db.ForeignKey('agency.id'))
+    fk_voyage_id = db.Column(db.Integer, db.ForeignKey('voyage.id'))
+    is_totally_paid = db.Column(db.Boolean, default=0)
+
+
+class Entity(db.Model):
+    __tablename__ = "entity"
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(100))
+    is_deleted = db.Column(db.Boolean, default=0)
+    montants = db.Column(db.Integer)
+    unit = db.Column(db.String(100))
+    quantity = db.Column(db.Integer, default=1)
+    total_amount = db.Column(db.Integer,default=0)
+    fk_invoice_id = db.Column(db.Integer, db.ForeignKey('invoice.id'))
+
+    def __repr__(self):
+        return f'{self.label}, {self.montants}, {self.unit}, {self.quantity}, {self.total_amount}'
 
